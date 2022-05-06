ced2ad503d31fb796d8dc4151a83ad402807c0aa
Ludovic <ludovic.mangaj@gmail.com>
2022-05-06 14:35:56 +0200

    we ended the small project

--- learn-react-toolkit-with-vite/src/App.tsx
+++ learn-react-toolkit-with-vite/src/App.tsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { useAppDispatch, useAppSelector } from './app/hooks'
+import { incremented, amountAdded } from './features/counter/counterSlice'
+import { useFetchBreedsQuery } from './features/dogs/dogs-apiSlice'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useAppSelector(state => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  const [numDogs, setNumDogs] = useState(10);
+  const { data = [], isFetching } = useFetchBreedsQuery(numDogs);
+
+  const handleClick = () => {
+    dispatch(amountAdded(4))
+  }
 
   return (
     <div className="App">
@@ -11,13 +22,40 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={handleClick}>
             count is: {count}
           </button>
         </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
+
+        <p>Dogs to fetch: </p>
+        <select value={numDogs} onChange={(e) => setNumDogs(Number(e.target.value))}>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+        </select>
+        <div>
+          Number of dogs fetched: {data.length}
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Picture</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((breed)=> (
+              <tr key={breed.id}>
+                <td>{breed.name}</td>
+                <td>
+                  <img src={breed.image.url} alt={breed.name} height = {250} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        
         <p>
           <a
             className="App-link"
